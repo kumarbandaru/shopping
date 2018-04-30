9e91d72fcaa489d309c11d4a098b60f49f329c23
SWAMY <SWAMY@KUMAR>
2018-04-30 13:40:23 -0700

    my project setup is completed

---
+++ kumarshoppingbackend/src/main/java/net/bks/kumarshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.bks.kumarshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
